4874d1c5d30a7cfd63b61a9faaed62a8dea44f38
AleksBulajic <aleks.bulajic25@gmail.com>
2024-08-01 15:56:17 -0400

    able to sign-in, sign-up, and sign-out with coockie session

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,6 +12,11 @@ authRouter.get("/sign-in", (req, res) => {
   res.render("auth/sign-in.ejs");
 });
 
+authRouter.get("/sign-out", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+});
+
 authRouter.post("/sign-up", async (req, res) => {
   const userInDatabase = await User.findOne({ username: req.body.username });
   if (userInDatabase) {
@@ -25,7 +30,12 @@ authRouter.post("/sign-up", async (req, res) => {
   req.body.password = hashedPassword;
 
   const user = await User.create(req.body);
-  res.send(`Thanks for signing up ${user.username}`);
+
+  if (user) {
+    res.redirect("/auth/sign-in");
+  } else {
+    res.send("Error occurred during sign up.");
+  }
 });
 
 authRouter.post("/sign-in", async (req, res) => {
@@ -35,13 +45,18 @@ authRouter.post("/sign-in", async (req, res) => {
       return res.send("Login failed. Please try again.");
     }
     //compare provided raw password with the hased password
-const validPassword = bcrypt.compareSync(
-    req.body.password,
-    userInDatabase.password
-)
-if (!validPassword) {
-    return res.send("Login failed. Please try again.");
-  }
+    const validPassword = bcrypt.compareSync(
+      req.body.password,
+      userInDatabase.password
+    );
+    if (!validPassword) {
+      return res.send("Login failed. Please try again.");
+    }
+    //create session for the user after he is signed-in
+    req.session.user = {
+      username: userInDatabase.username,
+    };
+    res.redirect("/");
   } catch (error) {
     console.error("was not able to sign in", error);
   }
--- server.js
+++ server.js
@@ -5,18 +5,15 @@ import express from "express";
 import mongoose from "mongoose";
 import methodOverride from "method-override";
 import morgan from "morgan";
+import session from "express-session";
 
 // controllers imports
-import authController from "./controllers/auth.js"
-
-
+import authController from "./controllers/auth.js";
 
 const app = express();
 
-
-
 // Set the port from environment variable or default to 3000
-const port = process.env.PORT
+const port = process.env.PORT;
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -24,28 +21,38 @@ mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-
-
-
 // Middleware to parse URL-encoded data from forms
 app.use(express.urlencoded({ extended: false }));
 // Middleware for using HTTP verbs such as PUT or DELETE
 app.use(methodOverride("_method"));
 // Morgan for logging HTTP requests
 app.use(morgan("dev"));
+//express-session
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-
-
-
-// GET 
+// GET
 app.get("/", async (req, res) => {
-    res.render("index.ejs")
+  res.render("index.ejs", {
+    user: req.session.user,
   });
+});
 
+app.get("/vip-lounge", (req, res) => {
+  if (req.session.user) {
+    res.send(`Welcome to the party ${req.session.user.username}.`);
+  } else {
+    res.send("Sorry, no guests allowed.");
+  }
+});
 
 //auth routes
 app.use("/auth", authController);
-
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
